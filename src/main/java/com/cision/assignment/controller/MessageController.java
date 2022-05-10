ee0ebda2b0d4292b0ecc1f5a25f5fb8a36b516ee
gergo-kiss <kiss.g.95@gmail.com>
2022-05-10 18:33:29 +0200

    Second endpoint to get persisted data

--- src/main/java/com/cision/assignment/controller/MessageController.java
+++ src/main/java/com/cision/assignment/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.cision.assignment.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -23,4 +25,13 @@ public class MessageController {
 		messageRepository.save(message);
         return true;
     }
+	
+	@RequestMapping(value = "/getmessages", method = RequestMethod.GET)
+	@ResponseBody
+    public List<Message> getMessages() {
+		List<Message> messages = new ArrayList<>();
+		messageRepository.findAll().forEach(messages::add);
+		return messages;
+	}
+	
 }
